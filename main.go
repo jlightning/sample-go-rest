70d507279edf25fef0e0675f262e079652bca1d1
minh.trannguyenhao <minh.trannguyenhao@lazada.com>
2018-05-28 13:50:57 +0700

    add config

--- main.go
+++ main.go
@@ -9,10 +9,23 @@ import (
 	"sample-go-rest/repositories"
 	"sample-go-rest/services"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-ini/ini"
 )
 
+type config struct {
+	database struct {
+		host     string
+		port     string
+		username string
+		password string
+	}
+}
+
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3307)/sample_rest?charset=utf8")
+	cfg := loadConfig()
+	dbConfig := cfg.database
+
+	db, err := sql.Open("mysql", dbConfig.username+":"+dbConfig.password+"@tcp("+dbConfig.host+":"+dbConfig.port+")/sample_rest?charset=utf8")
 	if err != nil {
 		panic(err)
 	}
@@ -30,4 +43,19 @@ func main() {
 
 	log.Println("Listening...")
 	http.ListenAndServe(":8080", mux)
+}
+
+func loadConfig() config {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	var configData config
+	configData.database.host = cfg.Section("database").Key("host").String()
+	configData.database.port = cfg.Section("database").Key("port").String()
+	configData.database.username = cfg.Section("database").Key("username").String()
+	configData.database.password = cfg.Section("database").Key("password").String()
+
+	return configData
 }
